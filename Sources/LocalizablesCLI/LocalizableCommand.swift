ce4cab6735897ec005aefa128b1bda384c7815c5
Jaume Corbi <jaume4@gmail.com>
2022-03-04 19:22:26 +0100

    allow read and minify only

--- Sources/LocalizablesCLI/LocalizableCommand.swift
+++ Sources/LocalizablesCLI/LocalizableCommand.swift
@@ -11,15 +11,43 @@ struct LocalizableCommand: ParsableCommand {
     var originFile: String
 
     @Argument(help: "Path to the target localizable file to update")
-    var destinationFile: String
+    var updatedFile: String
 
     mutating func run() throws {
         let originURL = URL(string: originFile.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!)!.fileURL
-        let destinationURL = URL(string: destinationFile.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!)!.fileURL
+        let updatedFileURL = URL(string: updatedFile.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!)!.fileURL
 
-        var parser = LiteralsUpdater(originURL: originURL, destinationURL: destinationURL)
+        var original = try LiteralsReader(url: originURL)
+        printInfo(literals: original, file: "origin")
 
-        try parser.run()
-        try parser.save()
+        let updates = try LiteralsReader(url: updatedFileURL)
+        printInfo(literals: updates, file: "updated")
+
+        let missingKeys = original.update(from: updates)
+            .sorted(by: { $0.caseInsensitiveCompare($1) == .orderedAscending })
+
+        if !missingKeys.isEmpty {
+            print("warning, missing keys found: \(missingKeys.count)")
+            print("\n----------------")
+            print(missingKeys.lazy.sorted().joined(separator: "\n"))
+            print("---------------\n")
+        }
+
+        try original.save()
+    }
+
+    func printInfo(literals: LiteralsReader, file: String) {
+        if !literals.duplicatedKeys.isEmpty {
+            print("warning, found \(literals.duplicatedKeys.count) duplicated keys found on \(file), unique keys: \(literals.keys.count)")
+            print("\n----------------")
+
+            let sortedDuplicates = literals.duplicatedKeys
+                .sorted(by: { $0.caseInsensitiveCompare($1) == .orderedAscending })
+
+            print(sortedDuplicates.joined(separator: "\n"))
+            print("---------------\n")
+        } else {
+            print("found \(literals.keys.count) on \(file)")
+        }
     }
 }
--- Sources/LocalizablesCore/LiteralsUpdater.swift
+++ Sources/LocalizablesCore/LiteralsUpdater.swift
@@ -6,76 +6,38 @@ import Parsing
 
 public typealias Literal = (key: String, value: String)
 
-public struct LiteralsUpdater {
-    let originURL: URL
-    let destinationURL: URL
-    var mergedLiterals: [Literal]
+public struct LiteralsReader {
+    public let url: URL
+    public private(set) var literals: [Literal]
+    public let duplicatedKeys: Set<String>
+    public let keys: Set<String>
 
-    public init(originURL: URL, destinationURL: URL) {
-        self.originURL = originURL
-        self.destinationURL = destinationURL
-        mergedLiterals = []
+    public init(url: URL) throws {
+        self.url = url
+
+        let string = try String(contentsOf: url)
+        literals = try LocalizablesParser.parse(from: string)
+            .sorted(by: { $0.key.caseInsensitiveCompare($1.key) == .orderedAscending })
+        (duplicatedKeys, keys) = Self.calculateDuplicates(from: literals)
     }
 
-    @discardableResult
-    public mutating func run() throws -> Info {
-        let destinationLiterals = try readLiterals(from: destinationURL)
-
-        print("found \(destinationLiterals.count) literals on destination file")
-
-        let (duplicatedKeys, destinationKeys) = calculateDuplicates(from: destinationLiterals)
-
-        let newLiterals = try readLiterals(from: originURL)
-
-        print("found \(newLiterals.count) literals on origin file")
-
-        let (mergedLiterals, missingKeys) = merge(destination: destinationLiterals,
-                                                  destinationKeys: destinationKeys,
-                                                  new: newLiterals)
-
-        self.mergedLiterals = mergedLiterals
-
-        let info = Info(duplicatedKeys: duplicatedKeys, updatedKeys: mergedLiterals, missingKeys: Array(missingKeys).sorted())
-
-        return info
-    }
-
-    func calculateDuplicates(from literals: [Literal]) -> (duplicates: [String], unique: Set<String>) {
-        var uniqueKeys: Set<String> = Set(minimumCapacity: literals.count)
-
-        let duplicatedKeys = literals
-            .map(\.key)
-            .filter { !uniqueKeys.insert($0).inserted }
-
-        if !duplicatedKeys.isEmpty {
-            print("warning, found \(duplicatedKeys.count) duplicated keys found, unique keys: \(uniqueKeys.count)")
-            print("\n----------------")
-            print(duplicatedKeys.joined(separator: "\n"))
-            print("---------------\n")
-        }
-
-        return (duplicatedKeys, uniqueKeys)
-    }
-
-    func merge(destination: [Literal], destinationKeys: Set<String>, new: [Literal]) -> (mergedLiterals: [Literal], missingKeys: Set<String>) {
+    public mutating func update(from origin: LiteralsReader) -> Set<String> {
         var foundKeys: Set<String> = []
-        var mergedLiterals: [Literal] = []
+        foundKeys.reserveCapacity(literals.count)
 
-        for (key, value) in new {
-            if destinationKeys.contains(key), foundKeys.insert(key).inserted {
+        var mergedLiterals: [Literal] = []
+        mergedLiterals.reserveCapacity(literals.count)
+
+        for (key, value) in origin.literals {
+            if keys.contains(key), foundKeys.insert(key).inserted {
                 mergedLiterals.append((key, value))
             }
         }
 
-        let missingKeys = destinationKeys.subtracting(foundKeys)
+        let missingKeys = keys.subtracting(foundKeys)
 
         if !missingKeys.isEmpty { // If we have missing literals, find them on the original one
-            print("warning, missing keys found: \(missingKeys.count)")
-            print("\n----------------")
-            print(missingKeys.lazy.joined(separator: "\n"))
-            print("---------------\n")
-
-            for (key, value) in destination {
+            for (key, value) in literals {
                 if foundKeys.insert(key).inserted {
                     mergedLiterals.append((key, value))
                 }
@@ -85,19 +47,16 @@ public struct LiteralsUpdater {
         let sorted = mergedLiterals
             .sorted(by: { $0.key.caseInsensitiveCompare($1.key) == .orderedAscending })
 
-        return (sorted, missingKeys)
-    }
+        literals = sorted
 
-    func readLiterals(from url: URL) throws -> [Literal] {
-        let string = try String(contentsOf: url)
-        return try LocalizablesParser.parse(from: string)
+        return missingKeys
     }
 
     public func save() throws {
-        try "".write(to: destinationURL, atomically: true, encoding: .utf8) // reset file
-        let handle = try FileHandle(forWritingTo: destinationURL)
+        try "".write(to: url, atomically: true, encoding: .utf8) // reset file
+        let handle = try FileHandle(forWritingTo: url)
 
-        for (key, value) in mergedLiterals {
+        for (key, value) in literals {
             let string = "\"\(key)\" = \"\(value)\";\n"
             if #available(macOS 10.15.4, *) {
                 try handle.write(contentsOf: Data(string.utf8))
@@ -108,12 +67,14 @@ public struct LiteralsUpdater {
 
         try handle.close()
     }
-}
 
-public extension LiteralsUpdater {
-    struct Info {
-        public let duplicatedKeys: [String]
-        public let updatedKeys: [Literal]
-        public let missingKeys: [String]
+    static func calculateDuplicates(from literals: [Literal]) -> (duplicates: Set<String>, unique: Set<String>) {
+        var uniqueKeys: Set<String> = Set(minimumCapacity: literals.count)
+
+        let duplicatedKeys = Set(literals
+            .map(\.key)
+            .filter { !uniqueKeys.insert($0).inserted })
+
+        return (duplicatedKeys, uniqueKeys)
     }
 }
