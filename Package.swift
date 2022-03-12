d11a00d5e6b388fc8c6a46274a206eee2680f526
Jaume Corbi <jaume4@gmail.com>
2022-03-12 19:08:34 +0100

    update swiftformat

--- Package.swift
+++ Package.swift
@@ -3,43 +3,35 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "LocalizablesTools",
-    platforms: [
-        .macOS(.v10_15),
-    ],
-    products: [
-        .executable(name: "update-localizables", targets: ["LocalizablesCLI"]),
-        .library(name: "LocalizablesCore", targets: ["LocalizablesCore"]),
-    ],
+let package = Package(name: "LocalizablesTools",
+                      platforms: [
+                          .macOS(.v10_15),
+                      ],
+                      products: [
+                          .executable(name: "update-localizables", targets: ["LocalizablesCLI"]),
+                          .library(name: "LocalizablesCore", targets: ["LocalizablesCore"]),
+                      ],
 
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.7.1"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LocalizablesCore",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Parsing", package: "swift-parsing"),
-            ]
-        ),
-        .executableTarget(
-            name: "LocalizablesCLI",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Parsing", package: "swift-parsing"),
-                "LocalizablesCore",
-            ]
-        ),
-        .testTarget(
-            name: "localizablesTests",
-            dependencies: ["LocalizablesCore"]
-        ),
-    ]
-)
+                      dependencies: [
+                          // Dependencies declare other packages that this package depends on.
+                          // .package(url: /* package url */, from: "1.0.0"),
+                          .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+                          .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.7.1"),
+                      ],
+                      targets: [
+                          // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+                          // Targets can depend on other targets in this package, and on products in packages this package depends on.
+                          .target(name: "LocalizablesCore",
+                                  dependencies: [
+                                      .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                                      .product(name: "Parsing", package: "swift-parsing"),
+                                  ]),
+                          .executableTarget(name: "LocalizablesCLI",
+                                            dependencies: [
+                                                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                                                .product(name: "Parsing", package: "swift-parsing"),
+                                                "LocalizablesCore",
+                                            ]),
+                          .testTarget(name: "localizablesTests",
+                                      dependencies: ["LocalizablesCore"]),
+                      ])
